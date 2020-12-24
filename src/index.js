dc57468f8c1c81a417e91f7d444fddde287a6078
Ruslan Horyn <ruslan.horyn95@gmail.com>
2020-12-24 09:50:10 +0100

    create: blog app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// import App from "./projeckts/3_css_in_react/App";
+import App from '../src/layouts/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
